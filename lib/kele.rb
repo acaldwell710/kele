1838c90cae2f14df0d9270956afc26c408d0380b
Amber Caldwell <acaldwell710@gmail.com>
2017-12-26 10:30:06 -0500

    added mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,8 +6,6 @@ class Kele
 
   def initialize(email, password)
     @api_url = "https://www.bloc.io/api/v1"
-
-    @auth = {email: email, password: password}
     @auth_token = "https://www.bloc.io/api/v1/sessions"
     response = Kele.post(@auth_token, body: {email: email, password: password})
     @auth_token = response["auth_token"]
@@ -16,6 +14,15 @@ class Kele
   def get_me
     response = Kele.get(
             "#{@api_url}/users/me",
+      headers: { "authorization" => @auth_token }
+    )
+    JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    mentor_id = 1329746
+    response = Kele.get(
+            "#{@api_url}/mentors/1329746/student_availability",
       headers: { "authorization" => @auth_token }
     )
     JSON.parse(response.body)
