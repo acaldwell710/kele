42d54d17d2987be6a5c3f69da9b858dc7cfc1a9b
Amber Caldwell <acaldwell710@gmail.com>
2018-01-08 17:28:00 -0500

    added checkpoint submission

--- lib/kele.rb
+++ lib/kele.rb
@@ -79,4 +79,15 @@ class Kele
       #@auth_token = response["auth_token"] <- gives 'nil'
       #JSON.parse(response.body) <- gives 'resource not found'
   end
+
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+    # 36133
+    response = self.class.post("#{@api_url}/checkpoint_submissions", body: {
+      checkpoint_id: checkpoint_id,
+      assignment_branch: assignment_branch,
+      assignment_commit_link: assignment_commit_link,
+      comment: comment,
+      enrollment_id: enrollment_id},
+      headers: {"authorization" => @auth_token})
+  end
 end
